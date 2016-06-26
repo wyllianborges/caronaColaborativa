ae52c3953a95729151169f9c55738e389b276b64
eduardohenzen <eduardohenzen@gmail.com>
2016-06-26 14:31:12 -0300

    actvity viangens cadastro

---
+++ app/src/main/java/br/com/up/caronaup/MinhasViagensCadastro.java
@@ -0,0 +1,13 @@
+package br.com.up.caronaup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MinhasViagensCadastro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_minhas_viagens_cadastro);
+    }
+}
